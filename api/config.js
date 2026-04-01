aecc3037d9abb43329be4f4b9ee17d42d2e9951a
jlk237 <35572109+jlk237@users.noreply.github.com>
2026-03-31 22:15:57 -0400

    Add files via upload

--- api/config.js
+++ api/config.js
@@ -1,6 +1,7 @@
 export default function handler(req, res) {
   res.status(200).json({
     supabaseUrl: process.env.SUPABASE_URL,
-    supabaseAnon: process.env.SUPABASE_ANON_KEY
+    supabaseAnon: process.env.SUPABASE_ANON_KEY,
+    stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY
   });
 }
